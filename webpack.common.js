9e3a50c33547aed88db2a4584bb88d94635feed5
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-02-21 15:14:25 +0100

    added base design
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- webpack.common.js
+++ webpack.common.js
@@ -8,15 +8,36 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 
 module.exports = {
-    entry: './src/index.js',
+    entry: path.resolve('src', 'index.js'),
     output: {
         path: path.resolve('dist', 'webclient'),
         filename: 'index_bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets:[ 'es2015', 'react', 'stage-2' ]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
         ]
     },
     plugins: [HtmlWebpackPluginConfig],
