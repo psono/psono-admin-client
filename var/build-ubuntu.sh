5572ae51c9fdb665206c6cf3bbca6669892e95a7
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-02-21 10:30:18 +0100

    another try
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/build-ubuntu.sh
+++ var/build-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get update && \
-apt-get install -y curl && \
+apt-get install -y curl apt-transport-https && \
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
 apt-get update && \
