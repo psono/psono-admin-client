59e97285a1484547ee7d34b4d583b574c82e4531
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-02-23 15:07:36 +0100

    added persistent store and redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+import { HashLoader } from 'react-spinners'
 import { createBrowserHistory } from 'history';
+
 import {
     Router,
     Route,
@@ -8,26 +16,48 @@ import {
 
 import 'chartist/dist/chartist.min.css';
 import 'chartist/dist/chartist.min.js';
-import '../assets/css/material-dashboard-react.css';
 
+import '../assets/css/material-dashboard-react.css';
 import indexRoutes from '../routes/index.jsx';
+import rootReducer from '../reducers'
 
 const hist = createBrowserHistory();
+const loggerMiddleware = createLogger();
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(
+    persistedReducer,
+    applyMiddleware(
+        thunkMiddleware, // lets us dispatch() functions
+        loggerMiddleware // neat middleware that logs actions
+    )
+);
+
+let persistor = persistStore(store);
+
 
 class App extends Component {
     render() {
         return (
-            <Router history={hist}>
-                <Switch>
-                    {
-                        indexRoutes.map((prop,key) => {
-                            return (
-                                <Route path={prop.path} component={prop.component}  key={key}/>
-                            );
-                        })
-                    }
-                </Switch>
-            </Router>
+            <PersistGate loading={<HashLoader />} persistor={persistor}>
+                <Router history={hist}>
+                    <Switch>
+                        {
+                            indexRoutes.map((prop,key) => {
+                                return (
+                                    <Route path={prop.path} render={()=><prop.component store={store} location={hist.location} />}  key={key}/>
+                                );
+                            })
+                        }
+                    </Switch>
+                </Router>
+            </PersistGate>
         );
     }
 }
