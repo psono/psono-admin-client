5430eeca75003a35cef470b2e6a51f1f771ccac0
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-07-15 09:14:07 +0200

    upgrade node and some libs
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/build-ubuntu.sh
+++ var/build-ubuntu.sh
@@ -3,7 +3,7 @@ apt-get update && \
 apt-get install -y curl apt-transport-https zip && \
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
-curl -sL https://deb.nodesource.com/setup_8.x | bash - && \
+curl -sL https://deb.nodesource.com/setup_10.x | bash - && \
 apt-get update && \
 apt-get -y install nodejs yarn && \
 yarn --version && \
