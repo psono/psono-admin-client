c896f6149c39468b1077574c48067274718f2d58
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-12-28 09:55:20 +0100

    added vulnerability scan
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/build-ubuntu.sh
+++ var/build-ubuntu.sh
@@ -8,7 +8,7 @@ npm --version && \
 npm config set registry https://psono.jfrog.io/psono/api/npm/npm/ && \
 npm config set @devexpress:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
 npm config set @types:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
-npm install && \
+npm ci && \
 npm install -g karma-cli && \
 npm run build && \
 ./var/update_version.sh && \
