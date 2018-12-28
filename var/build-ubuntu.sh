0fd6f5e093bf9e984c51ebe070dd5fd69a6044e6
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-12-28 09:17:56 +0100

    switch to npm
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/build-ubuntu.sh
+++ var/build-ubuntu.sh
@@ -3,14 +3,14 @@ apt-get update && \
 apt-get install -y curl apt-transport-https zip && \
 curl -sL https://deb.nodesource.com/setup_10.x | bash - && \
 apt-get update && \
-apt-get -y install nodejs yarn && \
+apt-get -y install nodejs && \
 npm --version && \
-yarn config set registry https://psono.jfrog.io/psono/api/npm/npm/ && \
-yarn config set @devexpress:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
-yarn config set @types:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
-yarn install && \
-yarn global add karma-cli -g && \
-yarn build && \
+npm config set registry https://psono.jfrog.io/psono/api/npm/npm/ && \
+npm config set @devexpress:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
+npm config set @types:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
+npm install && \
+npm install -g karma-cli && \
+npm run build && \
 ./var/update_version.sh && \
 cp LICENSE.md build/LICENSE.md
  
