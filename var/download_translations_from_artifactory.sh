7f7da92a72a91c42131ded6d0e6a4d6495d82b1c
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2019-05-11 08:07:42 +0200

    added some translations
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/download_translations_from_artifactory.sh
+++ var/download_translations_from_artifactory.sh
@@ -2,6 +2,13 @@
 # poeditor language codes: https://poeditor.com/docs/languages
 apt-get update && \
 apt-get install -y curl && \
+curl -o public/locales/da/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-da.json && \
+curl -o public/locales/sv/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-sv.json && \
+curl -o public/locales/no/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-no.json && \
+curl -o public/locales/he/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-he.json && \
+curl -o public/locales/ar/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-ar.json && \
+curl -o public/locales/hi/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-hi.json && \
+curl -o public/locales/bn/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-bn.json && \
 curl -o public/locales/cs/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-cs.json && \
 curl -o public/locales/de/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-de.json && \
 curl -o public/locales/en/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-en.json && \
@@ -15,6 +22,7 @@ curl -o public/locales/ko/translation.json https://psono.jfrog.io/psono/psono/ad
 curl -o public/locales/nl/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-nl.json && \
 curl -o public/locales/pl/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-pl.json && \
 curl -o public/locales/pt/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-pt.json && \
+curl -o public/locales/pt-BR/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-pt-br.json && \
 curl -o public/locales/ru/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-ru.json && \
 curl -o public/locales/vi/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-vi.json && \
 curl -o public/locales/zh-cn/translation.json https://psono.jfrog.io/psono/psono/admin-client/languages/locale-zh-cn.json
