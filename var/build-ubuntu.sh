7bdeecd6581271204d3b6503ae80d04ef8ee3b0a
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2019-05-15 18:57:50 +0200

    another try
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/build-ubuntu.sh
+++ var/build-ubuntu.sh
@@ -10,7 +10,7 @@ npm config set @devexpress:registry https://psono.jfrog.io/psono/api/npm/npm/ &&
 npm config set @types:registry https://psono.jfrog.io/psono/api/npm/npm/ && \
 npm ci && \
 npm install -g karma-cli && \
-INLINE_RUNTIME_CHUNK=false PUBLIC_URL=http://example.com/portal npm run build && \
+INLINE_RUNTIME_CHUNK=false npm run build && \
 ./var/update_version.sh && \
 cp LICENSE.md build/LICENSE.md
  
