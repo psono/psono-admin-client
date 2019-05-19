696a9685d3c0c7035c2869bc040283c3a262c9e2
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2019-05-19 11:19:00 +0200

    Fix #10 Added time convertion
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,8 @@ import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+// import i18next from 'i18next';
+// import moment from 'moment'
 
 i18n
     // load translation using xhr -> see /public/locales
