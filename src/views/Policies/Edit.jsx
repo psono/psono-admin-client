4a6188183caf4a723585835586218ab305ebbc24
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2026-05-15 13:13:30 +0200

    Added policy to disable multiple password datastores
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- src/views/Policies/Edit.jsx
+++ src/views/Policies/Edit.jsx
@@ -347,6 +347,10 @@ const PolicyEdit = (props) => {
 			type: "boolean",
 		},
 		{
+			key: "COMPLIANCE_DISABLE_MULTIPLE_PASSWORD_DATASTORES",
+			type: "boolean",
+		},
+		{
 			key: "COMPLIANCE_DISABLE_OFFLINE_MODE",
 			type: "boolean",
 		},
